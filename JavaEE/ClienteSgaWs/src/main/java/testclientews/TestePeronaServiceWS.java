fe0b5b51b7907631482956aa46e4815ba4b689f6
SeZamora <jorgesebastianz53@gmail.com>
2024-07-01 22:23:45 -0600

    WebServices con JAX-WS y API REST con JAX-RS en JEE

---
+++ JavaEE/ClienteSgaWs/src/main/java/testclientews/TestePeronaServiceWS.java
@@ -0,0 +1,21 @@
+package testclientews;
+
+import clientews.servicio.Persona;
+import clientews.servicio.PersonaServiceImplService;
+import clientews.servicio.PersonaServiceWs;
+import java.util.List;
+
+public class TestePeronaServiceWS {
+
+     public static void main(String[] args) {
+          PersonaServiceWs personaService = new PersonaServiceImplService().getPersonaServiceImplPort();
+
+          System.out.println("ejecutando servicio listar personas ws");
+          List<Persona> personas = personaService.listarPersonas();
+          for (Persona persona : personas) {
+               System.out.println("Persona idPersona:" + persona.getIdPersona() + ", nombre:" + persona.getNombre()
+                       + ", apellido:" + persona.getApellido() + ", email:" + persona.getEmail());
+          }
+          System.out.println("Fin servicio listar personas ws");
+     }
+}
